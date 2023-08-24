3345e14cf92093a026d4c52b896492df88ad1ec6
kiran-godse <82302395+kiran-godse@users.noreply.github.com>
2023-08-24 08:39:44 +0530

    Update generate-validate.js

--- generate-validate.js
+++ generate-validate.js
@@ -1,27 +1,27 @@
 const fs = require('fs');
 const jsonschema = require('jsonschema');
 
-// Load your predefined schema
+
 const schema = require('./schema.json'); 
 
-// Load discussion data from the GitHub Actions context
+
 const eventPath = process.argv[2];
 const discussionPayload = require(eventPath);
 
 // Debug: Print the event payload
-console.log('Event Payload:', discussionPayload);
+//console.log('Event Payload:', discussionPayload);
 
 // Extract discussion data from the payload
 const discussionTitle = discussionPayload.discussion.title;
 const discussionBody = discussionPayload.discussion.body;
 const discussionLabels = discussionPayload.discussion.labels ? discussionPayload.discussion.labels.map(label => label.name) : [];
 
-// Debug: Print the extracted discussion data
+
 console.log('Discussion Title:', discussionTitle);
 console.log('Discussion Body:', discussionBody);
 console.log('Discussion Labels:', discussionLabels);
 
-// Generate prompt JSON based on discussion
+
 const promptJson = {
   DiscussionTitle: discussionTitle,
   Labels: discussionLabels,
