2caa46e13e7833d8bd5ee4b263f685712b40fee6
kiran-godse <82302395+kiran-godse@users.noreply.github.com>
2023-08-25 16:19:23 +0530

    Update generate-validate.js

--- generate-validate.js
+++ generate-validate.js
@@ -1,36 +1,38 @@
 const fs = require('fs');
-
-// Load discussion data from the GitHub Actions context
+const jsonschema = require('jsonschema');
+const schema = require('./schema.json'); 
 const eventPath = process.argv[2];
 const discussionPayload = require(eventPath);
+// Debug: Print the event payload
+//console.log('Event Payload:', discussionPayload);
 
 // Extract discussion data from the payload
-const discussionTitle = discussionPayload.discussion.title;
-const discussionBody = discussionPayload.discussion.body;
+const Title = discussionPayload.discussion.title;
+const Body = discussionPayload.discussion.body;
+const Labels = discussionPayload.discussion.labels ? discussionPayload.discussion.labels.map(label => label.name) : [];
 
-// Function to extract labels from discussion body
-function extractLabelsFromDiscussionBody(body) {
-  const labelsRegex = /labels:\s*((?:[\w\s-]+,?\s*)+)/i;
-  const match = body.match(labelsRegex);
-  if (match && match[1]) {
-    const labelsString = match[1];
-    const labelsArray = labelsString.split(',').map(label => label.trim());
-    return labelsArray;
-  }
-  return [];
-}
 
-const discussionLabels = extractLabelsFromDiscussionBody(discussionBody);
+// console.log('Discussion Title:', discussionTitle);
+	@@ -23,9 +23,9 @@ const discussionLabels = discussionPayload.discussion.labels ? discussionPayload
 
-// Generate prompt JSON based on discussion
+
 const promptJson = {
-  Title: discussionTitle,
-  Labels: discussionLabels,
-  Body: discussionBody
+  Title: Title,
+  Labels: Labels,
+  Body: Body
 };
 
+// Debug: Print the contents of prompt.json
+console.log('Contents of prompt.json:\n', JSON.stringify(promptJson, null, 2));
 // Serialize promptJson to JSON format
 const promptJsonString = JSON.stringify(promptJson, null, 2);
-
 // Write promptJson to a file named prompt.json
 fs.writeFileSync('prompt.json', promptJsonString);
+// Validate against the schema
+const validationResult = jsonschema.validate(promptJson, schema);
+if (validationResult.valid) {
+  console.log('Generated prompt JSON is valid.');
+} else {
+  console.log('Generated prompt JSON is invalid.');
+  console.log('Validation errors:', validationResult.errors);
+}
